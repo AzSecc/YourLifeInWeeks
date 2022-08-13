80cae3d4731f824e798387566612cd04a907a3d7
r2sec <103225775+AzSecc@users.noreply.github.com>
2022-08-13 19:00:44 +0400

    Add files via upload

---
+++ your_life_in_counts.py
@@ -0,0 +1,5 @@
+die = int(input("How old do you think you will die > "))
+age = int(input("What is Your Current Age > "))
+remained_age = die - age
+
+print(f"You have {remained_age*365} days, {remained_age*52} weeks, {remained_age*12} months left.")
